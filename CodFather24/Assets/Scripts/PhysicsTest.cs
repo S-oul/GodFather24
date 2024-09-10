4f4bb1ea81920b1099956128c79781338592ac84
Sacha <S.Soulslasher@gmail.com>
2024-09-10 23:07:02 +0200

    [ADD] Anchors in PhysicsTest

--- CodFather24/Assets/Scripts/PhysicsTest.cs
+++ CodFather24/Assets/Scripts/PhysicsTest.cs
@@ -1,58 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PhysicsTest : MonoBehaviour
 {
+    [SerializeField] float _playerSpeed = .15f;
+    [SerializeField] float _bombPowerScale = 5;
+
     Rigidbody2D _rigidbody;
-    [SerializeField] float Scale = .15f;
-    [SerializeField] float bombPowerScale = 5;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();   
-        _rigidbody.velocity = new Vector2(Random.Range(0,15), Random.Range(0,15));
-        Debug.Log(_rigidbody.velocity);
     }
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Vector3 MouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0;
 
-            Vector2 DirToPlayer = MouseWorldPos - transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            if (hit && hit.transform.CompareTag("Anchor")) {
 
-            _rigidbody.velocity = DirToPlayer * Scale;
-            print(MouseWorldPos + " // " + DirToPlayer);
+                Vector2 mouseToPlayer = mouseWorldPos - transform.position;
+                float distance = Vector3.Distance(mouseWorldPos, transform.position);
+
+                _rigidbody.velocity = mouseToPlayer.normalized * _playerSpeed * Mathf.Max(0,distance * (1 / (distance + 1)));
+                print(mouseWorldPos + " // " + mouseToPlayer);
+            }
         }
     }
-
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("bomb"))
         {
             Vector2 dirToPlayer = transform.position - collision.transform.position;
-            _rigidbody.velocity = dirToPlayer * bombPowerScale;
+            _rigidbody.velocity = dirToPlayer * _bombPowerScale;
         }
     }
 
-
-
-
-
-
-
-
-
-
-
+    #region Debug
     private void OnDrawGizmos()
     {
-        Debug.DrawLine(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position, Color.red);
-        Debug.DrawLine(transform.position, transform.position + (Vector3)_rigidbody.velocity, Color.yellow);
-
+        if (Application.isPlaying)
+        {
+            Debug.DrawLine(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position, Color.red);
+            Debug.DrawLine(transform.position, transform.position + (Vector3)_rigidbody.velocity, Color.yellow);
+        }
     }
+#endregion  
 }
