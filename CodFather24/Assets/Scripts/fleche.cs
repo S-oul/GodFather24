cac664452be448877599748283a57ee3f01442f2
Margot <margotdelage02@gmail.com>
2024-09-11 17:30:28 +0200

    [MOD] fleche

--- CodFather24/Assets/Scripts/fleche.cs
+++ CodFather24/Assets/Scripts/fleche.cs
@@ -8,6 +8,8 @@ public class fleche : MonoBehaviour
 
     RectTransform transformFleche;
 
+    float speedRotation = 0.1f;
+
     private void Start()
     {
         transformFleche = fleche_aliment.GetComponent<RectTransform>();
@@ -22,6 +24,22 @@ public class fleche : MonoBehaviour
         
 
         transformFleche.position = new Vector2( Mathf.Clamp(screenPosition.x, 0, Camera.main.pixelWidth), Mathf.Clamp(screenPosition.y, 0, Camera.main.pixelHeight));
+
+
+        transform.eulerAngles += new Vector3(0,0, speedRotation);
+
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(this.transform.position);
+        if (viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0)
+        {
+            fleche_aliment.SetActive(false);
+        }
+        else
+        {
+            fleche_aliment.SetActive(true);
+        }
+
     }
+
 
 }
